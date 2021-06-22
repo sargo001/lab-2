5825c3a97f83833dab5e8c034c2d87a1d8dc0bcf
sargo001 <61858818+sargo001@users.noreply.github.com>
2021-06-22 13:39:43 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace laba_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Лабораторная работа 2. Массивы 2:Вывести количество положительных элементов.");
+            int v = 32;
+            Console.WriteLine("                                " + ((v % 15) + 1) + " Вариант");
+            Console.Write("Введите количество элементов:");
+            var len = int.Parse(0 + Console.ReadLine());
+
+
+            int count = 0;
+            var array = new int[len];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(i + 1 + " Элемент = ");
+                array[i] = int.Parse(Console.ReadLine());
+
+
+            }
+            Console.Write("Количество положительных элементов в массиве: ");
+
+            for (int i = 0; i < array.Length; i++)
+
+
+                if (array[i] > 0)
+
+
+
+                    count ++;
+
+
+
+
+            Console.Write(count);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
